d62bb50c4f9bfb2d47e0492064694784f9982e09
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2023-12-12 00:55:07 +0100

    Day 11 - part 2

--- src/jorgen/aoc23/dec11.clj
+++ src/jorgen/aoc23/dec11.clj
@@ -1,5 +1,6 @@
 (ns jorgen.aoc23.dec11
   (:require
+    [clojure.set :as set]
     [jorgen.aoc23.util :as util]))
 
 
@@ -32,30 +33,74 @@
        util/transpose))
 
 
-(defn distance [[x1 y1] [x2 y2]]
-  (+ (- (max x1 x2)
-        (min x1 x2))
-     (- (max y1 y2)
-        (min y1 y2))))
+(defn index-of-empty [grid]
+  (loop [idx-of-empty []
+         idx-lines (map-indexed vector grid)]
+    (let [[idx line] (first idx-lines)]
+      (if (empty? idx-lines)
+        (set idx-of-empty)
+        (if (every? #{"."} line)
+          (recur (conj idx-of-empty idx) (rest idx-lines))
+          (recur idx-of-empty (rest idx-lines)))))))
 
 
-(defn part1 [lines]
+(defn idx-range [x1 x2]
+  (set (if (< x1 x2)
+         (range x1 (inc x2))
+         (range x2 (inc x1)))))
+
+
+(defn galaxy-distance [x1 x2 empty-idxs old-factor]
+  (let [idx-to-cross (idx-range x1 x2)
+        distance (- (max x1 x2)
+                    (min x1 x2))
+        no-of-empty-to-cross (count (set/intersection empty-idxs idx-to-cross))
+        total-distance (+ (* old-factor no-of-empty-to-cross)
+                          (- distance no-of-empty-to-cross))]
+    total-distance))
+
+
+(defn total-galaxy-distance [empty-cols empty-rows old-factor [x1 y1] [x2 y2]]
+  (+ (galaxy-distance x1 x2 empty-cols old-factor)
+     (galaxy-distance y1 y2 empty-rows old-factor)))
+
+
+(defn solve
+  "Expand (or don't expand) the galaxy.
+   Find the positions of all the galaxies and store them in a map
+   galaxy-no => galaxy position.
+   Loop over all galaxy-no pairs and calculate distance between
+   all galaxy-pairs.
+  "
+  [lines expand-fn age-factor]
   (let [grid (util/parse-grid-of-chars lines)
-        expanded (expand-universe grid)
+        expanded (expand-fn grid)
         galaxy-positions (util/positions-of-char-in-grid "#" expanded)
         galaxy-map (into {} (map-indexed vector galaxy-positions))
-        galaxy-pairs (pairs (range (count galaxy-positions)))]
+        galaxy-pairs (pairs (range (count galaxy-positions)))
+        empty-columns (index-of-empty (util/transpose grid))
+        empty-rows (index-of-empty grid)
+        long-distance (partial total-galaxy-distance empty-columns empty-rows age-factor)]
     (->> galaxy-pairs
          (map (fn [[p1 p2]]
-                (distance (get galaxy-map p1)
-                          (get galaxy-map p2))))
+                (long-distance (get galaxy-map p1)
+                               (get galaxy-map p2))))
          (reduce +))))
+
+
+(defn part1
+  "Expansion and age-factor 1"
+  [lines]
+  (solve lines expand-universe 1))
+
+
+(defn part2
+  "No expansion and age-factor 1 000 000"
+  [lines]
+  (solve lines identity 1000000))
 
 
 (comment
   (time (part1 (util/file->lines "dec11_sample.txt")))
-  (time (part1 (util/file->lines "dec11_input.txt"))))
-;(time (part2 (util/file->lines "dec11_sample.txt")))
-;(time (part2 (util/file->lines "dec11_input.txt"))))
-
-
+  (time (part1 (util/file->lines "dec11_input.txt")))
+  (time (part2 (util/file->lines "dec11_input.txt"))))
--- test/jorgen/aoc23/dec11_test.clj
+++ test/jorgen/aoc23/dec11_test.clj
@@ -7,9 +7,8 @@
 
 (deftest testing-galaxy-expansion
   (testing "It should calculate the sum of all distances"
-    (let [sample (util/file->lines "dec11_sample.txt")
-          input (util/file->lines "dec11_input.txt")]
-      (is (= 374 (part1 sample)))
-      (is (= 9418609 (part1 input))))))
+    (is (= 374 (part1 (util/file->lines "dec11_sample.txt"))))
+    (is (= 9418609 (part1 (util/file->lines "dec11_input.txt"))))
+    (is (= 593821230983 (part2 (util/file->lines "dec11_input.txt"))))))
 
 
