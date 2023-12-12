cdcd01aec6bec8011f0954174b61d4cf08d3bac0
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2023-12-12 18:03:26 +0100

    Day 07 - part 1

--- src/jorgen/aoc23/dec07.clj
+++ src/jorgen/aoc23/dec07.clj
@@ -3,61 +3,33 @@
     [clojure.string :as str]
     [jorgen.aoc23.util :as util]))
 
+
 (def cards "AKQJT98765432")
-(def card-order (seq cards))
 
 
 (defn card-comp [c1 c2]
   (cond
-    (< (str/index-of cards c1) (str/index-of cards c2)) -1
-    (> (str/index-of cards c1) (str/index-of cards c2)) 1
+    (> (str/index-of cards c1) (str/index-of cards c2)) -1
+    (< (str/index-of cards c1) (str/index-of cards c2)) 1
     :else 0))
 
 
-(str "(?:" (str/join "|" (map #(str %1 "{" 5 "}") card-order)) ")")
-
-(def hand-strength
-  {:five-of-a-kind 7
-   :four-of-a-kind 6
-   :full-house 5
-   :three-of-a-kind 4
-   :two-pair 3
-   :one-pair 1
-   :high-card 0})
-
-
 (defn sort-hand [hand]
-  (sort-by identity card-comp hand))
+  (->> (sort-by identity card-comp hand)
+       str/join))
 
-(re-matches #"\w{5,}" "AAAA5")
 
 (defn hand-type [hand]
   (let [shand (sort-hand hand)]
     (cond
-      (re-matches #"\w{5,}" hand))))
+      (re-matches #"(\w)\1{4}" shand) 7
+      (re-matches #"\w?(\w)\1{3}\w?" shand) 6
+      (re-matches #"(?:(\w)\1{2}(\w)\2)|(?:(\w)\3(\w)\4{2})" shand) 5
+      (re-matches #"\w*?(\w)\1{2}\w*?" shand) 4
+      (re-matches #"\w?(\w)\1\w?(\w)\2\w?" shand) 3
+      (re-matches #"\w*?(\w)\1\w*?" shand) 2
+      :else 1)))
 
-(defn hand-comp [h1 h2]
-  (let [s1 (sort-hand h1)
-        s2 (sort-hand h2)]))
-
-
-(def hs
-  [{:hand "32T3K", :bet "765"}
-   {:hand "T55J5", :bet "684"}
-   {:hand "KK677", :bet "28"}
-   {:hand "KTJJT", :bet "220"}
-   {:hand "QQQJA", :bet "483"}])
-
-(->> hs
-     (map #(apply str (map (zipmap card-order (range)) (:hand %)))))
-(apply str (map (zipmap card-order (range)) (:hand (last hs))))
-
-(let [lines (util/file->lines "dec07_sample.txt")
-      hands (->> lines
-                 (map #(str/split % #"\s"))
-                 (mapv (fn [[hand bet]]
-                         (assoc {} :hand hand :bet bet))))]
-  hands)
 
 (defn highest-card [h1 h2]
   (->> (map vector h1 h2)
