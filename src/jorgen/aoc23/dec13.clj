9dc87a01fc0a3884b3e5f169f54c11599540d59a
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2023-12-13 23:51:59 +0100

    WIP

--- src/jorgen/aoc23/dec13.clj
+++ src/jorgen/aoc23/dec13.clj
@@ -23,16 +23,19 @@
     idx))
 
 
-(defn find-mirror-idx [grid]
-  (->> (find-symmetry-indicies grid)
+(defn find-mirror-idx [indicies grid]
+  (->> indicies
        (map #(symmetric-at-idx grid %))
        (remove nil?)
        first))
 
 
 (defn pattern-value [grid]
-  (let [h-mirror-idx (find-mirror-idx grid)
-        v-mirror-idx (find-mirror-idx (util/transpose grid))]
+  (let [h-indicies (find-symmetry-indicies grid)
+        h-mirror-idx (find-mirror-idx h-indicies grid)
+        t-grid (util/transpose grid)
+        v-indicies (find-symmetry-indicies t-grid)
+        v-mirror-idx (find-mirror-idx v-indicies t-grid)]
     (cond
       h-mirror-idx (* 100 h-mirror-idx)
       v-mirror-idx v-mirror-idx
@@ -45,6 +48,43 @@
        (map util/parse-grid-of-chars)
        (map pattern-value)
        (reduce +)))
+
+
+(defn find-smudge-with-idxs [symmetry-indicies grid]
+  (->> symmetry-indicies
+       (map #(split-at % grid))
+       (map (fn [[l r]] [(reverse l) r]))
+       (map (fn [[l r]] (map vector l r)))
+       (map (remove (fn [[l r]]
+                      (prn "l" l)
+                      (prn "r" r)
+                      (= l r))))))
+       ;empty?))
+
+(let [grid (->> (util/file->lines "dec13_sample.txt")
+                (util/parse-blocks)
+                (map util/parse-grid-of-chars)
+                first)]
+  (clojure.pprint/pprint (find-smudge-with-idxs [3] grid)))
+
+
+(defn find-smudge [grid]
+  (prn "##¤#¤¤#¤#¤#########")
+  (let [h-indicies (find-symmetry-indicies grid)
+        _ (prn "h-indicies" h-indicies)
+        h-mirror-idx (find-mirror-idx h-indicies grid)
+        _ (prn "h-mirror-idx" h-mirror-idx)
+        t-grid (util/transpose grid)
+        v-indicies (find-symmetry-indicies t-grid)
+        _ (prn "v-indicies" v-indicies)
+        v-mirror-idx (find-mirror-idx v-indicies t-grid)
+        _ (prn "v-mirror-idx" v-mirror-idx)]
+    (cond
+      (not h-mirror-idx) (find-smudge-with-idxs h-indicies grid)
+      (not v-mirror-idx) (find-smudge-with-idxs v-indicies t-grid)
+      ;h-mirror-idx (* 100 h-mirror-idx)
+      ;v-mirror-idx v-mirror-idx
+      :else (assert false "No symmetry, vertical or horisontal."))))
 
 
 (comment
