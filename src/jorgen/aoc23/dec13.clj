fccc7ba184fac13b4a35d40c188cedf29360bd4b
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2023-12-14 17:53:56 +0100

    WIP

--- src/jorgen/aoc23/dec13.clj
+++ src/jorgen/aoc23/dec13.clj
@@ -26,8 +26,8 @@
 (defn find-mirror-idx [indicies grid]
   (->> indicies
        (map #(symmetric-at-idx grid %))
-       (remove nil?)
-       first))
+       (remove nil?)))
+;first))
 
 
 (defn pattern-value [grid]
@@ -50,26 +50,49 @@
        (reduce +)))
 
 
+(defn diffs-idx [l r]
+  (->> (map vector l r)
+       (map-indexed (fn [idx [l r]]
+                      (when (not= l r)
+                        idx)))
+       (remove nil?)))
+
+
+
 (defn find-smudge-with-idxs [symmetry-indicies grid]
   (->> symmetry-indicies
-       (map #(split-at % grid))
+       (map #(split-at % (map-indexed vector grid)))
        (map (fn [[l r]] [(reverse l) r]))
        (map (fn [[l r]] (map vector l r)))
-       (map (remove (fn [[l r]]
-                      (prn "l" l)
-                      (prn "r" r)
-                      (= l r))))))
-       ;empty?))
+       (map (fn [lps] (remove (fn [[[i1 l] [i2 r]]] (= l r)) lps)))
+       (map (fn [lps]
+              ;(clojure.pprint/pprint lps)
+              ;;(assert (= 1 (count lps)))
+              (let [[[[i1 l] [i2 r]]] lps
+                    diffs (diffs-idx l r)]
+                (when (= 1 (count diffs))
+                  [i1 (first diffs)]))))
+       (remove nil?)
+       first
+       ((fn [o] (prn "oooo" o) o))))
 
-(let [grid (->> (util/file->lines "dec13_sample.txt")
-                (util/parse-blocks)
-                (map util/parse-grid-of-chars)
-                first)]
-  (clojure.pprint/pprint (find-smudge-with-idxs [3] grid)))
+
+(defn toggle-char [grid pos]
+  (update-in grid pos #(if (= % "#") "." "#")))
 
 
-(defn find-smudge [grid]
-  (prn "##¤#¤¤#¤#¤#########")
+(defn find-symmetry-indicies [grid]
+  (->> grid
+       (partition 2 1)
+       (map-indexed vector)
+       (reduce (fn [acc [i [l1 l2]]]
+                 (if (= l1 l2)
+                   (conj acc (inc i))
+                   acc))
+               [])))
+
+(defn correct-smudge [grid]
+  (prn "##€€#€#€€€#€##€€#")
   (let [h-indicies (find-symmetry-indicies grid)
         _ (prn "h-indicies" h-indicies)
         h-mirror-idx (find-mirror-idx h-indicies grid)
@@ -80,11 +103,50 @@
         v-mirror-idx (find-mirror-idx v-indicies t-grid)
         _ (prn "v-mirror-idx" v-mirror-idx)]
     (cond
-      (not h-mirror-idx) (find-smudge-with-idxs h-indicies grid)
-      (not v-mirror-idx) (find-smudge-with-idxs v-indicies t-grid)
-      ;h-mirror-idx (* 100 h-mirror-idx)
-      ;v-mirror-idx v-mirror-idx
-      :else (assert false "No symmetry, vertical or horisontal."))))
+      (not h-mirror-idx) (->> (find-smudge-with-idxs h-indicies grid)
+                              (toggle-char grid))
+      (not v-mirror-idx) (->> (find-smudge-with-idxs v-indicies t-grid)
+                              (toggle-char t-grid)
+                              util/transpose)
+      :else (assert false "No smudge?"))))
+
+
+
+#_(let [[a b] (split-at 3 first-block)
+        _ (clojure.pprint/pprint a)
+        _ (clojure.pprint/pprint b)]
+    (every? identity (map = (reverse a) b)))
+
+(map-indexed vector [6 5 4 3 2])
+
+
+
+(let [grids (->> (util/file->lines "dec13_input.txt")
+                 (util/parse-blocks)
+                 (map util/parse-grid-of-chars)
+                 vec)
+      _ (prn "(count grids)" (count grids))]
+      ;_ (clojure.pprint/pprint grids)]
+  ;_ (clojure.pprint/pprint (util/transpose (second grids)))
+  ;_ (prn "================")
+  ;second-block (second (->> (util/file->lines "dec13_sample.txt")
+  ;                          (util/parse-blocks)))
+  ;_ (clojure.pprint/pprint second-block)
+  ;_ (prn "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUu")]
+  (count (->> grids
+          ;(map util/transpose)
+          (map #(find-smudge-with-idxs (range 1 (count %)) %))
+          (remove nil?)
+          (map (fn [[l r]] (inc l))))))
+            ;(reduce +)
+            ;(* 100))))
+       #_(->> grids
+              (map util/transpose)
+              (map #(find-smudge-with-idxs (range 1 (count %)) %))
+              (remove nil?)
+              (map (fn [[l r]] (inc l)))))
+            ;(reduce +)
+            ;(* 100))))
 
 
 (comment
