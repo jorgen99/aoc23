b5dce252c52b4af1224ad9b7ce10c2f5108c7e6f
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2023-12-16 16:41:33 +0100

    Inactivated tests for non started parts

--- test/jorgen/aoc23/dec14_test.clj
+++ test/jorgen/aoc23/dec14_test.clj
@@ -5,13 +5,13 @@
     [jorgen.aoc23.dec14 :refer :all]))
 
 
-(deftest testing-
-  (testing "It should "
-    (is (= 1 (part1 (util/file->lines "dec14_sample.txt"))))
-    (is (= 1 (part1 (util/file->lines "dec14_input.txt"))))))
-
-
-  (testing "It should "
-    (is (= 1 (part (util/file->lines "dec14_sample.txt"))))
-    (is (= 1 (part2 (util/file->lines "dec14_input.txt"))))))
+;(deftest testing-
+;  (testing "It should "
+;    (is (= 1 (part1 (util/file->lines "dec14_sample.txt"))))
+;    (is (= 1 (part1 (util/file->lines "dec14_input.txt"))))))
+;
+;
+;  (testing "It should "
+;    (is (= 1 (part (util/file->lines "dec14_sample.txt"))))
+;    (is (= 1 (part2 (util/file->lines "dec14_input.txt"))))))
 
---
+++ test/jorgen/aoc23/dec16_test.clj
@@ -0,0 +1,17 @@
+(ns jorgen.aoc23.dec16-test
+  (:require
+    [clojure.test :refer :all]
+    [jorgen.aoc23.util :as util]
+    [jorgen.aoc23.dec16 :refer :all]))
+
+
+;(deftest testing-
+;  (testing "It should "
+;    (is (= 1 (part1 (util/file->lines "dec16_sample.txt"))))
+;    (is (= 1 (part1 (util/file->lines "dec16_input.txt"))))))
+;
+;
+;  ;(testing "It should "
+;  ;  (is (= 1 (part (util/file->lines "dec16_sample.txt"))))
+;  ;  (is (= 1 (part2 (util/file->lines "dec16_input.txt"))))))
+
