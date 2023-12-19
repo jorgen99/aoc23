b4cc84d3e8585f8025e13d4359ee625822483cf1
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2023-12-19 23:26:13 +0100

    Day 19 - part 1

--- src/jorgen/aoc23/dec19.clj
+++ src/jorgen/aoc23/dec19.clj
@@ -1,34 +1,76 @@
 (ns jorgen.aoc23.dec19
   (:require
     [clojure.string :as str]
+    [clojure.edn :as edn]
     [jorgen.aoc23.util :as util]))
 
 
+(defn ->rule [rule-parts]
+  (let [parsed-parts (map (fn [r] (re-matches #"(\w)(.)(\d+)\:(\w+)" r)) (butlast rule-parts))
+        pp (map (fn [part]
+                  (let [[category op value next-rule] (rest part)]
+                    {:category  (keyword category)
+                     :op        (ns-resolve 'clojure.core (symbol op))
+                     :threshold (util/parse-int value)
+                     :next-rule (keyword next-rule)}))
+                parsed-parts)]
+    {:rules pp :default-rule (keyword (last rule-parts))}))
 
-
-
-
-;(defn parse-parts [part-strs]
-;  (reduce (fn [acc part]
-;            (str/replace part #""))))
 
 (defn parse-workflow [workflow]
-  (prn workflow)
-  (let [name (first (str/split workflow #"\{"))
+  (let [name (->> (str/split workflow #"\{")
+                  first
+                  keyword)
         between (second (re-matches #".*?\{(.*?)\}" workflow))
         rule-parts (str/split between #",")]
+    [name (->rule rule-parts)]))
 
-    rule))
 
-(let [lines (util/file->lines "dec19_sample.txt")
-      blocks (util/parse-blocks lines)]
-  (->> (first blocks)
-       (map parse-workflow)))
+(defn parse-part [part-def]
+  (->> (str/replace part-def #"[,=]" " ")
+       edn/read-string
+       (map (fn [[k v]] [(keyword k) v]))
+       (into {})))
+
+
+(defn one-step-in-flow [{:keys [rules default-rule]} part]
+  (loop [rs rules]
+    (if (empty? rs)
+      default-rule
+      (let [{:keys [category op threshold next-rule]} (first rs)]
+        (if (op (category part) threshold)
+          next-rule
+          (recur (rest rs)))))))
+
+
+(defn accepted? [workflow part]
+  (loop [rule (:in workflow)]
+    (let [next-rule (one-step-in-flow rule part)]
+      (case next-rule
+        (:A :R) next-rule
+        (recur (next-rule workflow))))))
+
+
+(defn part1 [lines]
+  (let [blocks (util/parse-blocks lines)
+        workflow (->> (first blocks)
+                      (map parse-workflow)
+                      (into {}))
+        parts (->> (second blocks)
+                   (map parse-part))]
+    (->> parts
+         (map (partial accepted? workflow))
+         (map vector parts)
+         (filter #(= :A (second %)))
+         (map first)
+         (map vals)
+         flatten
+         (reduce +))))
 
 
 (comment
-  (time (part1 (util/file->lines "dec19_sample.txt"))))
-;(time (part1 (util/file->lines "dec19_input.txt")))
+  (time (part1 (util/file->lines "dec19_sample.txt")))
+  (time (part1 (util/file->lines "dec19_input.txt"))))
 ;(time (part2 (util/file->lines "dec19_sample.txt")))
 ;(time (part2 (util/file->lines "dec19_input.txt"))))
 
--- test/jorgen/aoc23/dec19_test.clj
+++ test/jorgen/aoc23/dec19_test.clj
@@ -5,12 +5,12 @@
     [jorgen.aoc23.dec19 :refer :all]))
 
 
-;(deftest testing-
-;  (testing "It should "
-;    (is (= 1 (part1 (util/file->lines "dec19_sample.txt"))))
-;    (is (= 1 (part1 (util/file->lines "dec19_input.txt"))))))
-;
-;
+(deftest testing-
+  (testing "It should "
+    (is (= 19114 (part1 (util/file->lines "dec19_sample.txt"))))
+    (is (= 342650 (part1 (util/file->lines "dec19_input.txt"))))))
+
+
 ;  ;(testing "It should "
 ;  ;  (is (= 1 (part (util/file->lines "dec19_sample.txt"))))
 ;  ;  (is (= 1 (part2 (util/file->lines "dec19_input.txt"))))))
